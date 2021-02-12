af83f1c78256188fd5ca8bf4e1123c60ab09c559
blascopuch <pabloblascoaragon23@gmail.com>
2021-02-12 14:00:28 +0100

    Creadas entidades

---
+++ src/com/company/entities/Usuario.java
@@ -0,0 +1,16 @@
+package com.company.entities;
+
+public class Usuario {
+
+    private String alias;
+    private String passwd;
+
+    public Usuario(String alias, String passwd) {
+        this.alias = alias;
+        this.passwd = passwd;
+    }
+
+
+}
+
+public static void principal(){}
